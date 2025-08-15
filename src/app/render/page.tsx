a8a814b8b8e6f9fae89352e78611985bd4711e42
rutdvaj <54937822+rutdvaj@users.noreply.github.com>
2025-08-15 16:06:38 +0530

    DC-4

--- src/app/render/page.tsx
+++ src/app/render/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { AppSidebar } from "@/components/app-sidebar";
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
